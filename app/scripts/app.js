3dbee6af0d73add953a0f490d046a94b19b30ff7
postlain <dgroba@mail.ru>
2017-01-27 16:53:29 +0300

    parallax config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,44 +4,4 @@ import $ from 'jquery';
 $(() => {
 	svg4everybody();
 });
-$('body').parallax({
-	elements: [
-		{
-			selector: 'div.yellow',
-			properties: {
-				x: {
-					'background-position-x': {
-						initial: 0,
-						multiplier: 0.1,
-						invert: true
-					}
-				},
-				y: {
-					'background-position-y': {
-						initial: 0,
-						multiplier: 0.1,
-						invert: true
-					}
-				}
-			}
-		},
 
-		{
-			selector: 'div.blue',
-			properties: {
-				x: {
-					'background-position-x': {
-						initial: 0,
-						multiplier: 0.3
-					}
-				},
-				y: {
-					'background-position-y': {
-						initial: 0,
-						multiplier: 0.3
-					}
-				}
-			}
-		}
-	]
-});
